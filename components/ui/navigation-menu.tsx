70f753babeab177d62cfdcf7e4fbc5c52df1be9d
andreyscott <andreyscott301@gmail.com>
2024-05-30 23:14:41 +0100

    worked 4

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -7,7 +7,7 @@ import { FaAngleDown } from "react-icons/fa";
 import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu"
 import { cva } from "class-variance-authority"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils" 
 
 const NavigationMenu = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Root>,
